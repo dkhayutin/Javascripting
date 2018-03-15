f7b49f204843d69ce85b3bbdaf6d5328efd8f717
Dan <dan.khayutin@gmail.com>
2018-03-15 02:45:30 +0000

    Initial commit with all my work so far

---
+++ javascripting/functions.js
@@ -0,0 +1,4 @@
+  function eat(food){
+console.log(food + " tasted really good.")
+  }
+eat("bananas")
---
+++ javascripting/if-statement.js
@@ -0,0 +1,5 @@
+var fruit = 'orange';
+console.log('The fruit name has more than five characters.')
+if(fruit.length < 5){
+  console.log('The fruit name has 5 characters or less')
+}
